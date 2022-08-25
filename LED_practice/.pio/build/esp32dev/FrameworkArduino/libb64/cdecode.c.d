821a94ac1229dd2abb105d29795afab0d6112c4d
Luftalian <springtechtree@gmail.com>
2022-08-25 16:33:48 +0900

    delete unneeded file

--- LED_practice/.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/sprin/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- C:/Users/sprin/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
