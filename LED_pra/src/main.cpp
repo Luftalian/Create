409fa94108c861beb5ac089b2b65749adf04c676
Luftalian <springtechtree@gmail.com>
2022-08-25 23:13:31 +0900

    From ubuntu data

---
+++ LED_pra/src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+#include <SPICREATE.h>
+#include <H3LIS331.h>
+
+#define H3LIS331CS 26
+#define SPIFREQ 12000000
+
+H3LIS331 H3lis331;
+
+SPICREATE::SPICreate SPI;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("launched");
+
+  SPI.begin();
+  //センサのbeginをかく。
+  // void begin(SPICREATE::SPICreate *targetSPI, int cs, uint32_t freq = 8000000);
+  H3lis331.begin(&SPI, H3LIS331CS, SPIFREQ);
+  // WhoamIをかく
+  int a;
+  a = H3lis331.WhoImI();
+  Serial.print("WhoAmI:");
+  Serial.println(a);
+  if (a ==)
+  {
+    Serial.println("H3LIS331 is OK");
+  }
+  else
+  {
+    Serial.println("H3LIS331 is NG");
+  }
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
