5199fa97f66272b9bdfc8dfe73a01ad8321a0e64
Luftalian <springtechtree@gmail.com>
2022-08-25 13:49:55 +0900

    First IMPORTANT Commit

---
+++ LED_practice/.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/sprin/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/sprin/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/sprin/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
