4a9bf33bac9f4a475e06105d7f1fb74da44d9de3
Luftalian <springtechtree@gmail.com>
2022-09-10 21:34:06 +0900

    Revert "Revert "2022/09/10""
    This reverts commit c16c01878326d4ac64fa75e8ec456308041cacf0.

---
+++ SPIflash_S25FL127S/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /home/haruki/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/haruki/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/haruki/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/haruki/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
