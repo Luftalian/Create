eba329403a015e0a00fd1ac6a0f635e68ef062e6
Luftalian <springtechtree@gmail.com>
2022-09-10 21:23:48 +0900

    2022/09/10

--- READ_log_board_bata/src/main.cpp
+++ READ_log_board_bata/src/main.cpp
@@ -1,10 +1,77 @@
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+// DO NOT WRITE ERASE CODE HERE
+// DO NOT WRITE ERASE CODE HERE
+// DO NOT WRITE ERASE CODE HERE
+// DO NOT WRITE ERASE CODE HERE
+// DO NOT WRITE ERASE CODE HERE
+// DO NOT WRITE ERASE CODE HERE
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+
 #include <Arduino.h>
-#include <fstream>
+#include <SPICREATE.h>
+#include <S25FL512S.h>
+// #include <H3LIS331.h>
+
+#define flashCS 27
+#define SCK1 33
+#define MISO1 25
+#define MOSI1 26
+// #define H3LIS331CS 32
+
+bool WhoAmI_Ok = false;
+uint8_t count = 0;
+uint8_t tx[256] = {};
+int16_t rx[3] = {};
+
+// int CountH3LIS331DataSetExistInBuff = 0;
+int CountH3LIS331DataSetExistInBuff_READ = 0;
+
+// uint8_t H3LIS331FlashBuff[256] = {0};
+
+// uint32_t H3LIS331FlashLatestAddress = 0x00;
+uint32_t H3LIS331FlashLatestAddress_READ = 0x00;
+
+#define SPIFREQ 100000
+
+// H3LIS331 H3lis331;
+
+SPICREATE::SPICreate SPIC1;
+Flash flash1;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  SPIC1.begin(VSPI, SCK1, MISO1, MOSI1);
+  flash1.begin(&SPIC1, flashCS, SPIFREQ);
+  // H3lis331.begin(&SPIC1, H3LIS331CS, SPIFREQ);
+  // put your setup code here, to run once:
+
+  // WhoAmI
+  // uint8_t a;
+  // a = H3lis331.WhoImI();
+  // Serial.print("WhoAmI:");
+  // Serial.println(a);
+  // if (a == 0x32)
+  // {
+  //   Serial.println("H3LIS331 is OK");
+  //   WhoAmI_Ok = true;
+  // }
+  // else
+  // {
+  //   Serial.println("H3LIS331 is NG");
+  // }
+  Serial.println("400g: r, Acc: a, Gyro: g, Mag: m, LPS: l");
 }
 
 void loop()
@@ -12,25 +79,304 @@ void loop()
   // put your main code here, to run repeatedly:
   if (Serial.available())
   {
-    Serial.println("start");
     char cmd = Serial.read();
     if (cmd == 'r')
     {
-      std::fstream fileStream;
-      fileStream.open("example.txt");
-      if (fileStream.fail())
+      Serial.println("r is pressed");
+      // read Section
+      tx[256] = {};
+      rx[3] = {};
+      // Serial.println("time,x,y,z");
+      Serial.println("x,y,z");
+      while (1)
       {
-        // file could not be opened
-        Serial.println("file could not be opened");
+        flash1.read(H3LIS331FlashLatestAddress_READ, tx);
+        if (tx[0] == 255)
+        {
+          Serial.println("No Data");
+          for (int i = 0; i < 5; i++)
+          {
+            Serial.print(".");
+            delay(1000);
+          }
+          Serial.println();
+          break;
+        }
+        for (int CountH3LIS331DataSetExistInBuff = 0; CountH3LIS331DataSetExistInBuff < 8; CountH3LIS331DataSetExistInBuff++)
+        {
+          //時間をとる
+          // Serial.println("time");
+          // for (int i = 1; i < 5; i++)
+          // {
+          //   rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 0];
+          //   rx[0] |= (tx[32 * CountH3LIS331DataSetExistInBuff + i]) << (8 * i);
+          // }
+          // Serial.print(rx[0]);
+          // Serial.print(",");
+
+          //加速度をとる
+          // Serial.println("acceleration");
+          rx[3] = {};
+          rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 4];
+          rx[0] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 5]) << 8;
+          rx[1] = tx[32 * CountH3LIS331DataSetExistInBuff + 6];
+          rx[1] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 7]) << 8;
+          rx[2] = tx[32 * CountH3LIS331DataSetExistInBuff + 8];
+          rx[2] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 9]) << 8;
+          for (int i = 0; i < 3; i++)
+          {
+            Serial.print(rx[i]);
+            if (i != 2)
+            {
+              Serial.print(",");
+            }
+          }
+          Serial.println();
+          CountH3LIS331DataSetExistInBuff_READ++;
+          if (CountH3LIS331DataSetExistInBuff_READ == 8)
+          {
+            H3LIS331FlashLatestAddress_READ += 0x100;
+            CountH3LIS331DataSetExistInBuff_READ = 0;
+          }
+        }
       }
-      else
+    }
+    if (cmd == 'a')
+    {
+      Serial.println("a is pressed");
+      // read Section
+      tx[256] = {};
+      rx[3] = {};
+      Serial.println("time,x,y,z");
+      while (1)
       {
-        Serial.println("file could be opened");
+        flash1.read(H3LIS331FlashLatestAddress_READ, tx);
+        if (tx[0] == 255)
+        {
+          Serial.println("No Data");
+          for (int i = 0; i < 5; i++)
+          {
+            Serial.print(".");
+            delay(1000);
+          }
+          Serial.println();
+          break;
+        }
+        for (int CountH3LIS331DataSetExistInBuff = 0; CountH3LIS331DataSetExistInBuff < 8; CountH3LIS331DataSetExistInBuff++)
+        {
+          //時間をとる
+          // Serial.println("time");
+          for (int i = 1; i < 5; i++)
+          {
+            rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 0];
+            rx[0] |= (tx[32 * CountH3LIS331DataSetExistInBuff + i]) << (8 * i);
+          }
+          Serial.print(rx[0]);
+          Serial.print(",");
+
+          // MPUの加速度をとる
+          rx[3] = {};
+          rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 10];
+          rx[0] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 11]) << 8;
+          rx[1] = tx[32 * CountH3LIS331DataSetExistInBuff + 12];
+          rx[1] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 13]) << 8;
+          rx[2] = tx[32 * CountH3LIS331DataSetExistInBuff + 14];
+          rx[2] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 15]) << 8;
+          for (int i = 0; i < 3; i++)
+          {
+            Serial.print(rx[i]);
+            if (i != 2)
+            {
+              Serial.print(",");
+            }
+          }
+          Serial.println();
+          CountH3LIS331DataSetExistInBuff_READ++;
+          if (CountH3LIS331DataSetExistInBuff_READ == 8)
+          {
+            H3LIS331FlashLatestAddress_READ += 0x100;
+            CountH3LIS331DataSetExistInBuff_READ = 0;
+          }
+        }
       }
-      std::ofstream myfile;
-      myfile.open("example.txt");
-      myfile << "Writing this to a file.\n";
-      myfile.close();
+    }
+    if (cmd == 'g')
+    {
+      Serial.println("g is pressed");
+      // read Section
+      tx[256] = {};
+      rx[3] = {};
+      Serial.println("time,x,y,z");
+      while (1)
+      {
+        flash1.read(H3LIS331FlashLatestAddress_READ, tx);
+        if (tx[0] == 255)
+        {
+          Serial.println("No Data");
+          for (int i = 0; i < 5; i++)
+          {
+            Serial.print(".");
+            delay(1000);
+          }
+          Serial.println();
+          break;
+        }
+        for (int CountH3LIS331DataSetExistInBuff = 0; CountH3LIS331DataSetExistInBuff < 8; CountH3LIS331DataSetExistInBuff++)
+        {
+          //時間をとる
+          // Serial.println("time");
+          for (int i = 1; i < 5; i++)
+          {
+            rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 0];
+            rx[0] |= (tx[32 * CountH3LIS331DataSetExistInBuff + i]) << (8 * i);
+          }
+          Serial.print(rx[0]);
+          Serial.print(",");
+
+          // MPUの角速度をとる
+          rx[3] = {};
+          rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 16];
+          rx[0] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 17]) << 8;
+          rx[1] = tx[32 * CountH3LIS331DataSetExistInBuff + 18];
+          rx[1] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 19]) << 8;
+          rx[2] = tx[32 * CountH3LIS331DataSetExistInBuff + 20];
+          rx[2] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 21]) << 8;
+          for (int i = 0; i < 3; i++)
+          {
+            Serial.print(rx[i]);
+            if (i != 2)
+            {
+              Serial.print(",");
+            }
+          }
+          Serial.println();
+          CountH3LIS331DataSetExistInBuff_READ++;
+          if (CountH3LIS331DataSetExistInBuff_READ == 8)
+          {
+            H3LIS331FlashLatestAddress_READ += 0x100;
+            CountH3LIS331DataSetExistInBuff_READ = 0;
+          }
+        }
+      }
+    }
+    if (cmd == 'm')
+    {
+      Serial.println("m is pressed");
+      // read Section
+      tx[256] = {};
+      rx[3] = {};
+      Serial.println("time,x,y,z");
+      while (1)
+      {
+        flash1.read(H3LIS331FlashLatestAddress_READ, tx);
+        if (tx[0] == 255)
+        {
+          Serial.println("No Data");
+          for (int i = 0; i < 5; i++)
+          {
+            Serial.print(".");
+            delay(1000);
+          }
+          Serial.println();
+          break;
+        }
+        for (int CountH3LIS331DataSetExistInBuff = 0; CountH3LIS331DataSetExistInBuff < 8; CountH3LIS331DataSetExistInBuff++)
+        {
+          //時間をとる
+          // Serial.println("time");
+          for (int i = 1; i < 5; i++)
+          {
+            rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 0];
+            rx[0] |= (tx[32 * CountH3LIS331DataSetExistInBuff + i]) << (8 * i);
+          }
+          Serial.print(rx[0]);
+          Serial.print(",");
+
+          // MPUの地磁気をとる
+          rx[3] = {};
+          rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 22];
+          rx[0] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 23]) << 8;
+          rx[1] = tx[32 * CountH3LIS331DataSetExistInBuff + 24];
+          rx[1] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 25]) << 8;
+          rx[2] = tx[32 * CountH3LIS331DataSetExistInBuff + 26];
+          rx[2] |= ((uint16_t)tx[32 * CountH3LIS331DataSetExistInBuff + 27]) << 8;
+          for (int i = 0; i < 3; i++)
+          {
+            Serial.print(rx[i]);
+            if (i != 2)
+            {
+              Serial.print(",");
+            }
+          }
+          Serial.println();
+          CountH3LIS331DataSetExistInBuff_READ++;
+          if (CountH3LIS331DataSetExistInBuff_READ == 8)
+          {
+            H3LIS331FlashLatestAddress_READ += 0x100;
+            CountH3LIS331DataSetExistInBuff_READ = 0;
+          }
+        }
+      }
+    }
+    if (cmd == 'l')
+    {
+      Serial.println("l is pressed");
+      // read Section
+      tx[256] = {};
+      rx[3] = {};
+      Serial.println("time,x,y,z");
+      while (1)
+      {
+        flash1.read(H3LIS331FlashLatestAddress_READ, tx);
+        if (tx[0] == 255)
+        {
+          Serial.println("No Data");
+          for (int i = 0; i < 5; i++)
+          {
+            Serial.print(".");
+            delay(1000);
+          }
+          Serial.println();
+          break;
+        }
+        for (int CountH3LIS331DataSetExistInBuff = 0; CountH3LIS331DataSetExistInBuff < 8; CountH3LIS331DataSetExistInBuff++)
+        {
+          //時間をとる
+          // Serial.println("time");
+          for (int i = 1; i < 5; i++)
+          {
+            rx[0] = tx[32 * CountH3LIS331DataSetExistInBuff + 0];
+            rx[0] |= (tx[32 * CountH3LIS331DataSetExistInBuff + i]) << (8 * i);
+          }
+          Serial.print(rx[0]);
+          Serial.print(",");
+
+          /// LPSの気圧をとる
+          for (int index = 28; index < 31; index++)
+          {
+            Serial.print(tx[32 * CountH3LIS331DataSetExistInBuff + index]);
+            if (index != 30)
+            {
+              Serial.print(",");
+            }
+          }
+          for (int i = 0; i < 3; i++)
+          {
+            Serial.print(rx[i]);
+            if (i != 2)
+            {
+              Serial.print(",");
+            }
+          }
+          Serial.println();
+          CountH3LIS331DataSetExistInBuff_READ++;
+          if (CountH3LIS331DataSetExistInBuff_READ == 8)
+          {
+            H3LIS331FlashLatestAddress_READ += 0x100;
+            CountH3LIS331DataSetExistInBuff_READ = 0;
+          }
+        }
+      }
     }
   }
 }
---
+++ SPIflash_S25FL127S/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ /home/haruki/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /home/haruki/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
