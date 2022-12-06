59ef2a86ef12d7264acd8b9cc97a84a22d662af7
Luftalian <springtechtree@gmail.com>
2022-12-06 09:59:10 +0900

    2022/12/06 Serial2

--- Serial2/src/main.cpp
+++ Serial2/src/main.cpp
@@ -6,24 +6,32 @@ void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(100);
   Serial2.begin(9600);
+  delay(100);
+  Serial.println("start Serial");
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  while (Serial2.available())
+  // Serial2.write('e');
+  // while (Serial2.available())
+  // {
+  char receive;
+  receive = Serial2.read();
+  Serial2.write(receive);
+  Serial.println(receive);
+  delay(1000);
+  Serial.println("a");
+  if (receive == 'e')
   {
-    char receive;
-    receive = Serial2.read();
-    Serial.write(receive);
-    if (receive == 'e')
-    {
-      Serial2.println("OK");
-      digitalWrite(LED1, HIGH);
-      delay(100);
-      digitalWrite(LED1, LOW);
-      delay(100);
-    }
+    Serial2.println("OK");
+    Serial.println("OK");
+    digitalWrite(LED1, HIGH);
+    delay(100);
+    digitalWrite(LED1, LOW);
+    delay(100);
   }
+  // }
 }
