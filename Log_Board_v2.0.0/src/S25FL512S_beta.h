6eb25e748d73ffd094863d71f4c2cbf77f0b21bf
Luftalian <springtechtree@gmail.com>
2023-02-10 14:24:00 +0900

    chenge a littile

--- Log_Board_v2.0.0/src/S25FL512S_beta.h
+++ Log_Board_v2.0.0/src/S25FL512S_beta.h
@@ -172,7 +172,7 @@ uint32_t Flash::setFlashAddress()
 
 void Flash::erase()
 {
-    // Serial.println("start erase");
+    Serial.println("start erase");
     if (flashSPI == NULL)
     {
         return;
@@ -187,7 +187,7 @@ void Flash::erase()
         Serial.print(",");
         delay(100);
     }
-    // Serial.println("Bulk Erased");
+    Serial.println("Bulk Erased");
     return;
 }
 void Flash::write(uint32_t addr, uint8_t *tx)
